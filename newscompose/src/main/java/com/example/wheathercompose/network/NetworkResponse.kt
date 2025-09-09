7d9dc912ba140f900c157e007ddc88642cd521ed
surabhiDixit <dixit1694@gmail.com>
2025-09-09 20:00:35 +0530

    - newscompose module - sealed class with Type T ,out, in understanding

--- newscompose/src/main/java/com/example/wheathercompose/network/NetworkResponse.kt
+++ newscompose/src/main/java/com/example/wheathercompose/network/NetworkResponse.kt
@@ -1,4 +1,6 @@
 package com.example.wheathercompose.network
+// understanding of type T , out, in , return type how to create and call these function
+//https://betulnecanli.medium.com/kotlin-generics-in-out-where-terms-with-examples-445dc0bb45d6
 
 sealed class NetworkResponse<out T> {
     data class Success<out T>(val data: T):NetworkResponse<T>()
